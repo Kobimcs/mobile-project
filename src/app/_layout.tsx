37863e1a0905f17397c16606cf0f219c40cb811f
Pa Alieu Njie <212911333+Pa-Alieu25@users.noreply.github.com>
2026-06-10 21:46:51 +0000

    Organize app navigation routes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,19 +3,24 @@ import { Stack } from 'expo-router';
 export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="exam-venue-search" />
-      <Stack.Screen name="manage-exam-venues" />
-      <Stack.Screen name="manage-timetable" />
-      <Stack.Screen name="timetable" />
-      <Stack.Screen name="assignments" />
-      <Stack.Screen name="add-assignment" />
-      <Stack.Screen name="post-announcement" />
-      <Stack.Screen name="announcements" />
+      {/* Auth screens */}
       <Stack.Screen name="index" />
       <Stack.Screen name="register" />
       <Stack.Screen name="forgot-password" />
+
+      {/* Student screens */}
       <Stack.Screen name="student-dashboard" />
+      <Stack.Screen name="timetable" />
+      <Stack.Screen name="announcements" />
+      <Stack.Screen name="assignments" />
+      <Stack.Screen name="exam-venue-search" />
+
+      {/* Course rep screens */}
       <Stack.Screen name="rep-panel" />
+      <Stack.Screen name="post-announcement" />
+      <Stack.Screen name="add-assignment" />
+      <Stack.Screen name="manage-timetable" />
+      <Stack.Screen name="manage-exam-venues" />
     </Stack>
   );
 }
